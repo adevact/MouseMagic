8185fd8384534942ff4ac900168c94408f34f68b
Arthur <yesyes.arthur@gmail.com>
2022-06-01 00:03:35 +0800

    Add project files.

---
+++ AutoMouseClick/MainForm.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace AutoMouseClick
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cpbProgress = new AutoMouseClick.CircularProgressBar();
+            this.tmrCountDown = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblMouseInactiveTimeoutMin = new System.Windows.Forms.Label();
+            this.ncMouseActivityTimeout = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.ncCountDown = new System.Windows.Forms.NumericUpDown();
+            this.tmrCountDownMouseMovTimeout = new System.Windows.Forms.Timer(this.components);
+            this.lblSystemMsg = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ncMouseActivityTimeout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ncCountDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(18, 37);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(240, 79);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(18, 135);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(240, 79);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cpbProgress
+            // 
+            this.cpbProgress.BackColor = System.Drawing.SystemColors.Control;
+            this.cpbProgress.BarColor1 = System.Drawing.Color.SeaShell;
+            this.cpbProgress.BarColor2 = System.Drawing.Color.OliveDrab;
+            this.cpbProgress.BarWidth = 25F;
+            this.cpbProgress.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cpbProgress.ForeColor = System.Drawing.Color.DimGray;
+            this.cpbProgress.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            this.cpbProgress.LineColor = System.Drawing.Color.Transparent;
+            this.cpbProgress.LineWidth = 1;
+            this.cpbProgress.Location = new System.Drawing.Point(295, 37);
+            this.cpbProgress.Maximum = ((long)(300));
+            this.cpbProgress.MinimumSize = new System.Drawing.Size(100, 100);
+            this.cpbProgress.Name = "cpbProgress";
+            this.cpbProgress.ProgressShape = AutoMouseClick.CircularProgressBar._ProgressShape.Flat;
+            this.cpbProgress.Size = new System.Drawing.Size(176, 176);
+            this.cpbProgress.TabIndex = 2;
+            this.cpbProgress.Text = "300";
+            this.cpbProgress.TextMode = AutoMouseClick.CircularProgressBar._TextMode.Value;
+            this.cpbProgress.Value = ((long)(300));
+            // 
+            // tmrCountDown
+            // 
+            this.tmrCountDown.Interval = 1000;
+            this.tmrCountDown.Tick += new System.EventHandler(this.tmrCountDown_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Click Interval (s):";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cpbProgress);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Location = new System.Drawing.Point(12, 195);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(489, 237);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblMouseInactiveTimeoutMin);
+            this.groupBox2.Controls.Add(this.ncMouseActivityTimeout);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lblMinutes);
+            this.groupBox2.Controls.Add(this.ncCountDown);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(489, 163);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Setting";
+            // 
+            // lblMouseInactiveTimeoutMin
+            // 
+            this.lblMouseInactiveTimeoutMin.AutoSize = true;
+            this.lblMouseInactiveTimeoutMin.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMouseInactiveTimeoutMin.Location = new System.Drawing.Point(371, 103);
+            this.lblMouseInactiveTimeoutMin.MinimumSize = new System.Drawing.Size(100, 0);
+            this.lblMouseInactiveTimeoutMin.Name = "lblMouseInactiveTimeoutMin";
+            this.lblMouseInactiveTimeoutMin.Size = new System.Drawing.Size(100, 25);
+            this.lblMouseInactiveTimeoutMin.TabIndex = 5;
+            this.lblMouseInactiveTimeoutMin.Text = "(5 min)";
+            // 
+            // ncMouseActivityTimeout
+            // 
+            this.ncMouseActivityTimeout.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ncMouseActivityTimeout.Increment = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.ncMouseActivityTimeout.Location = new System.Drawing.Point(252, 96);
+            this.ncMouseActivityTimeout.Maximum = new decimal(new int[] {
+            6000,
+            0,
+            0,
+            0});
+            this.ncMouseActivityTimeout.Minimum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.ncMouseActivityTimeout.Name = "ncMouseActivityTimeout";
+            this.ncMouseActivityTimeout.ReadOnly = true;
+            this.ncMouseActivityTimeout.Size = new System.Drawing.Size(113, 39);
+            this.ncMouseActivityTimeout.TabIndex = 4;
+            this.ncMouseActivityTimeout.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.ncMouseActivityTimeout.ValueChanged += new System.EventHandler(this.ncMouseActivityTimeout_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mouse Activity Timeout (s):";
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMinutes.Location = new System.Drawing.Point(372, 47);
+            this.lblMinutes.MinimumSize = new System.Drawing.Size(100, 0);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(100, 25);
+            this.lblMinutes.TabIndex = 2;
+            this.lblMinutes.Text = "(5 min)";
+            // 
+            // ncCountDown
+            // 
+            this.ncCountDown.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ncCountDown.Increment = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.ncCountDown.Location = new System.Drawing.Point(253, 40);
+            this.ncCountDown.Maximum = new decimal(new int[] {
+            6000,
+            0,
+            0,
+            0});
+            this.ncCountDown.Minimum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.ncCountDown.Name = "ncCountDown";
+            this.ncCountDown.ReadOnly = true;
+            this.ncCountDown.Size = new System.Drawing.Size(113, 39);
+            this.ncCountDown.TabIndex = 1;
+            this.ncCountDown.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.ncCountDown.ValueChanged += new System.EventHandler(this.ncCountDown_ValueChanged);
+            // 
+            // tmrCountDownMouseMovTimeout
+            // 
+            this.tmrCountDownMouseMovTimeout.Interval = 15000;
+            this.tmrCountDownMouseMovTimeout.Tick += new System.EventHandler(this.tmrCountDownMouseMovTimeout_Tick);
+            // 
+            // lblSystemMsg
+            // 
+            this.lblSystemMsg.AutoSize = true;
+            this.lblSystemMsg.Location = new System.Drawing.Point(12, 444);
+            this.lblSystemMsg.MinimumSize = new System.Drawing.Size(400, 0);
+            this.lblSystemMsg.Name = "lblSystemMsg";
+            this.lblSystemMsg.Size = new System.Drawing.Size(400, 25);
+            this.lblSystemMsg.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 478);
+            this.Controls.Add(this.lblSystemMsg);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Mouse Magic";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ncMouseActivityTimeout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ncCountDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button btnStart;
+        private Button btnStop;
+        private CircularProgressBar cpbProgress;
+        private System.Windows.Forms.Timer tmrCountDown;
+        private Label label1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private NumericUpDown ncCountDown;
+        private Label lblMinutes;
+        private System.Windows.Forms.Timer tmrCountDownMouseMovTimeout;
+        private Label lblMouseInactiveTimeoutMin;
+        private NumericUpDown ncMouseActivityTimeout;
+        private Label label3;
+        private Label lblSystemMsg;
+    }
+}
---
+++ AutoMouseClick/MainForm.cs
@@ -0,0 +1,220 @@
+using Gma.System.MouseKeyHook;
+using System.Runtime.InteropServices;
+
+namespace AutoMouseClick
+{
+
+    public partial class MainForm : Form
+    {
+        /* -- BEGIN -- */
+        /*
+         * Reference:   https://www.c-sharpcorner.com/article/timer-in-C-Sharp/
+         * Purpose:     Simulated mouse click without user input
+         */
+
+        // This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        // This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+        /* -- END -- */
+
+        /* -- BEGIN -- */
+        /*
+         * Reference    https://stackoverflow.com/questions/61817755/detect-mouse-clicks-outside-the-form-c-sharp
+         * Purpose:     Retrieve mouse X and Y position globally even outside the form
+         * Dependence:  Nuget package MouseKeyHook
+         */
+        private POINT oldMousePos;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        private void MouseMoveAll(object? sender, MouseEventArgs e)
+        {
+            POINT newMousePos;
+
+            if (GetCursorPos(out newMousePos))
+            {
+                // if mouse is moved, stop the count down timer, start the timeout count down timer
+                if (oldMousePos.X != newMousePos.X && oldMousePos.Y != newMousePos.Y)
+                {
+                    // record position to check if mouse is moved
+                    oldMousePos = newMousePos;
+
+                    // update position
+                    lblSystemMsg.Invoke((MethodInvoker)delegate
+                    {
+                        lblSystemMsg.ForeColor = Color.OrangeRed;
+                        lblSystemMsg.Text = "Mouse movement detected: [" + Cursor.Position.X + "," + Cursor.Position.Y + "]";
+                    });
+                    
+                    // reset timers
+                    tmrCountDown.Enabled = false;
+                    tmrCountDownMouseMovTimeout.Enabled = false;
+                    tmrCountDownMouseMovTimeout.Enabled = true;
+                }
+            }
+        }
+        /* -- END -- */
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Hook.GlobalEvents().MouseMove += MouseMoveAll;
+            oldMousePos = new POINT(Cursor.Position.X, Cursor.Position.Y);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Update progress bar and msg
+            cpbProgress.Invoke((MethodInvoker)delegate
+            {
+                cpbProgress.Value = (long)ncCountDown.Value;
+            });
+
+            lblSystemMsg.Invoke((MethodInvoker)delegate
+            {
+                lblSystemMsg.Text = "";
+            });
+
+            // Disable the numeric up down controls
+            ncCountDown.Enabled = false;
+            ncMouseActivityTimeout.Enabled = false;
+
+            // Start / Stop button
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            // Kick off timer tick
+            tmrCountDown.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Update progress bar and msg
+            cpbProgress.Invoke((MethodInvoker)delegate
+            {
+                cpbProgress.Value = (long)ncCountDown.Value;
+            });
+
+            lblSystemMsg.Invoke((MethodInvoker)delegate
+            {
+                lblSystemMsg.Text = "";
+            });
+
+            // Enable the numeric up down controls
+            ncCountDown.Enabled = true;
+            ncMouseActivityTimeout.Enabled = true;
+
+            // Start / Stop button
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+            // Disable CountDown timer and Mouse movement timeout timer
+            tmrCountDown.Enabled = false;
+            tmrCountDownMouseMovTimeout.Enabled = false;
+        }
+
+        private void tmrCountDown_Tick(object sender, EventArgs e)
+        {
+            // Update the count down every 1 second
+            // Simulate mouse click when count down to 0
+
+            if (cpbProgress.Value > 1)
+            {
+                cpbProgress.Value--;
+            }
+            else
+            {
+                // Update progress bar
+                cpbProgress.Invoke((MethodInvoker)delegate
+                {
+                    cpbProgress.Value = (long)ncCountDown.Value;
+                });
+
+                // Simulate left mouse click
+                int x = Cursor.Position.X;
+                int y = Cursor.Position.Y;
+
+                LeftMouseClick(x, y);
+            }
+        }
+
+        private void tmrCountDownMouseMovTimeout_Tick(object sender, EventArgs e)
+        {
+            // Timeout since last mouse movement reached, continue the count down
+            tmrCountDown.Enabled = true;
+
+            lblSystemMsg.Invoke((MethodInvoker)delegate
+            {
+                lblSystemMsg.Text = "";
+            });
+        }
+
+        private void ncCountDown_ValueChanged(object sender, EventArgs e)
+        {
+            // Update progress bar
+            cpbProgress.Invoke((MethodInvoker)delegate
+            {
+                cpbProgress.Maximum = (long)ncCountDown.Value;
+                cpbProgress.Value = (long)ncCountDown.Value;
+                cpbProgress.Text = cpbProgress.Value.ToString();
+            });
+
+            // Update count down in Minutes
+            lblMinutes.Invoke((MethodInvoker)delegate
+            {
+                lblMinutes.Text = "(" + ((long)ncCountDown.Value / 60) + " min)";
+            });
+        }
+        private void ncMouseActivityTimeout_ValueChanged(object sender, EventArgs e)
+        {
+            // Update mouse activity timeout
+            lblMouseInactiveTimeoutMin.Invoke((MethodInvoker)delegate
+            {
+                lblMouseInactiveTimeoutMin.Text = "(" + ((long)ncMouseActivityTimeout.Value / 60) + " min)";
+            });
+
+            // Update mouse activity timeout timer interval
+            tmrCountDownMouseMovTimeout.Interval = (int)ncMouseActivityTimeout.Value * 1000;
+        }
+    }
+}
